feed42935291af633723c9e2d6dbb532f19a7a62
장민수 <ms.jang@3secondz.com>
2019-03-13 20:12:07 +0900

    priority-donate-sema 수정, donate로 priority 바뀌는 거 대응 위해 한 번 더 sort pass tests/threads/alarm-single pass tests/threads/alarm-multiple pass tests/threads/alarm-simultaneous pass tests/threads/alarm-priority pass tests/threads/alarm-zero pass tests/threads/alarm-negative pass tests/threads/priority-change pass tests/threads/priority-donate-one pass tests/threads/priority-donate-multiple pass tests/threads/priority-donate-multiple2 FAIL tests/threads/priority-donate-nest pass tests/threads/priority-donate-sema FAIL tests/threads/priority-donate-lower pass tests/threads/priority-fifo pass tests/threads/priority-preempt pass tests/threads/priority-sema FAIL tests/threads/priority-condvar FAIL tests/threads/priority-donate-chain FAIL tests/threads/mlfqs-load-1 FAIL tests/threads/mlfqs-load-60 FAIL tests/threads/mlfqs-load-avg FAIL tests/threads/mlfqs-recent-1 pass tests/threads/mlfqs-fair-2 pass tests/threads/mlfqs-fair-20 FAIL tests/threads/mlfqs-nice-2 FAIL tests/threads/mlfqs-nice-10 FAIL tests/threads/mlfqs-block

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -117,6 +117,7 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   struct thread *t = NULL;
   if (!list_empty (&sema->waiters)) {
+      list_sort(&sema->waiters, thread_compare_priority, NULL);
       t = list_entry(list_pop_front(&sema->waiters), struct thread, elem);
       thread_unblock (t);
     }
